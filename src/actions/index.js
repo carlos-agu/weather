bbc218c12ea7d9d3b28fe9ccf2ac6e366816c53c
Carlos Aguirre <carlos.agu@gmail.com>
2017-09-18 20:15:05 -0600

    Se corrigio el URL del weather map

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,16 @@ const API_KEY = '023598509cad9e54f8194a6f091736b0';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city){
 
     const url = `${ROOT_URL}&q=${city},us`;
+    console.log(url);
+
     const request = axios.get(url);
+
+    console.log('Request: ', request);
     return {
         type: FETCH_WEATHER,
         payload: request

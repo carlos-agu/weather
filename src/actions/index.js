40eebed1544670c42d83cb42b48fef604fedaa2b
Carlos Aguirre <carlos.agu@gmail.com>
2017-09-27 15:06:50 -0600

    Termino del ejemplo Weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,11 +9,7 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export function fetchWeather(city){
 
     const url = `${ROOT_URL}&q=${city},us`;
-    console.log(url);
-
-    const request = axios.get(url);
-
-    console.log('Request: ', request);
+    const request = axios.get(url);    
     return {
         type: FETCH_WEATHER,
         payload: request
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,13 @@
-export default function(state=null, action){
-    console.log("Action: ", action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state=[], action){    
+    switch(action.type){
+        case FETCH_WEATHER:
+        //return state.push(action.payload.data);ERROR no se puede modificar o mutar state, se tiene
+        //que crear una nueva instancia de state en este caso es un arreglo
+            //return state.concat([action.payload.data]);El metodo concat regresa un nuevo arreglo.
+            return [action.payload.data, ...state]; //ES6 igual a arriba
+    }
     return state;
 }
 
